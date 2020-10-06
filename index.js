84ac3d20d95dedc37304bb3c73fb3cac8e6e53cb
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-06 19:54:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const tutorials = [
 ];
 
 const titleCased = () => tutorials.map(function(element) {
-  let arr =
-
   const str = element.split(" ").map(function(e) {
     return e.charAt(0).toUpperCase() + e.slice(1)
   })
